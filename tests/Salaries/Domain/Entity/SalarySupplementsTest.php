8e2151848d8423cb4ecaef524b0801aeadbcf130
Michał Kalinowski <kalinowski5@gmail.com>
2022-08-17 12:45:17 +0200

    providePercentageSupplementCases

--- tests/Salaries/Domain/Entity/SalarySupplementsTest.php
+++ tests/Salaries/Domain/Entity/SalarySupplementsTest.php
@@ -14,26 +14,33 @@ use XYZ\Salaries\Domain\ValueObject\PercentageSupplement;
 
 class SalarySupplementsTest extends TestCase
 {
-    public function testSalarySupplement(): void
+    /** @dataProvider providePercentageSupplementCases */
+    public function testPercentageSupplement(BaseSalary $baseSalary, PercentageSupplement $percentageSupplement, Money $expectedTotalSalary): void
     {
-        $department = new Department(
-            Uuid::fromString('867f03c4-edb6-47cc-9ea7-2f4f43b62be1'),
-            'Customer Service',
-            new PercentageSupplement(10)
-        );
+        $department = new Department(Uuid::fromString('867f03c4-edb6-47cc-9ea7-2f4f43b62be1'), 'Customer Service', $percentageSupplement);
 
         $employee = new Employee(
             Uuid::fromString('e3bfc3d9-cf7c-405e-84a2-0a1a9bed6539'),
             new EmployeeName('John', 'Doe'),
             new \DateTimeImmutable('2020-01-01'),
             $department,
-            new BaseSalary(Money::USD(1100_00)),
+            $baseSalary,
         );
 
-        //@TODO: use data provider
-        self::assertEquals(
-            Money::USD(1210_00),
-            $employee->totalSalaryAt(new \DateTimeImmutable('2020-02-01'))
-        );
+        $actualTotalSalary = $employee->totalSalaryAt(new \DateTimeImmutable('2020-02-01'));
+
+        self::assertEquals($expectedTotalSalary, $actualTotalSalary);
     }
+
+    public function providePercentageSupplementCases(): array
+    {
+        return [
+            [new BaseSalary(Money::USD(1100_00)), new PercentageSupplement(0), Money::USD(1100_00)],
+            [new BaseSalary(Money::USD(1100_00)), new PercentageSupplement(10), Money::USD(1210_00)],
+            [new BaseSalary(Money::EUR(1100_00)), new PercentageSupplement(10), Money::EUR(1210_00)],
+            [new BaseSalary(Money::USD(5000_00)), new PercentageSupplement(25), Money::USD(6250_00)],
+            [new BaseSalary(Money::USD(1000_00)), new PercentageSupplement(100), Money::USD(2000_00)],
+        ];
+    }
+
 }
