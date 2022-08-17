f1950ed0c2845559fe32c7bdbf8a04303151e183
Michał Kalinowski <kalinowski5@gmail.com>
2022-08-17 12:33:36 +0200

    Prepare DB schema before tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,19 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
+passthru(sprintf(
+    'php "%s/../bin/console" doctrine:database:drop --env=test --force --quiet',
+    __DIR__
+));
+passthru(sprintf(
+    'php "%s/../bin/console" doctrine:database:create --env=test --no-interaction --quiet',
+    __DIR__
+));
+passthru(sprintf(
+    'php "%s/../bin/console" doctrine:schema:create --env=test --quiet',
+    __DIR__
+));
+
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
