70a594a85111e89d005ea74f723082b9f3d561a5
Michał Kalinowski <kalinowski5@gmail.com>
2022-08-17 22:56:57 +0200

    Add more sample data

--- src/Salaries/UI/CLI/SeedExampleDataCommand.php
+++ src/Salaries/UI/CLI/SeedExampleDataCommand.php
@@ -36,8 +36,9 @@ final class SeedExampleDataCommand extends Command
         $departments = [
             new Department(Uuid::v4(), 'Human Resources', new PercentageSupplement(15)),
             new Department(Uuid::v4(), 'Customer Service', new PercentageSupplement(10)),
-            new Department(Uuid::v4(), 'Marketing', new SenioritySupplement(Money::USD(5000_00))),
+            new Department(Uuid::v4(), 'Marketing', new SenioritySupplement(Money::USD(500_00))),
             new Department(Uuid::v4(), 'IT', new PercentageSupplement(5)),
+            new Department(Uuid::v4(), 'Research and Development', new SenioritySupplement(Money::USD(300_00))),
         ];
         foreach ($departments as $department) {
             $this->entityManager->persist($department);
@@ -46,11 +47,22 @@ final class SeedExampleDataCommand extends Command
         $this->entityManager->flush();
 
         $employees = [
-            new Employee(Uuid::v4(), new EmployeeName('John', 'Doe'), new \DateTimeImmutable(), $departments[0], new BaseSalary(Money::USD(1900_00))),
-            new Employee(Uuid::v4(), new EmployeeName('John', 'Doe'), new \DateTimeImmutable(), $departments[0], new BaseSalary(Money::USD(2300_00))),
-            new Employee(Uuid::v4(), new EmployeeName('John', 'Doe'), new \DateTimeImmutable(), $departments[2], new BaseSalary(Money::USD(5000_00))),
-            new Employee(Uuid::v4(), new EmployeeName('John', 'Doe'), new \DateTimeImmutable(), $departments[3], new BaseSalary(Money::USD(3000_00))),
-            new Employee(Uuid::v4(), new EmployeeName('John', 'Doe'), new \DateTimeImmutable(), $departments[2], new BaseSalary(Money::USD(1000_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Zeshan', 'Hooper'), new \DateTimeImmutable('1 year ago'), $departments[0], new BaseSalary(Money::USD(1900_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Uzma', 'Adamson'), new \DateTimeImmutable('2 years ago'), $departments[0], new BaseSalary(Money::USD(4000_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Abbi', 'Doe'), new \DateTimeImmutable('10 years ago'), $departments[0], new BaseSalary(Money::USD(2300_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Javan', 'Chang'), new \DateTimeImmutable('7 years ago'), $departments[2], new BaseSalary(Money::USD(5000_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Jamil', 'Cox'), new \DateTimeImmutable('2 years ago'), $departments[2], new BaseSalary(Money::USD(2900_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Pascal', 'Oliver'), new \DateTimeImmutable('2 months ago'), $departments[3], new BaseSalary(Money::USD(3000_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Leslie', 'Doe'), new \DateTimeImmutable('1 month ago'), $departments[2], new BaseSalary(Money::USD(1000_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Rayhan', 'Busby'), new \DateTimeImmutable('3 months ago'), $departments[4], new BaseSalary(Money::USD(1900_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Carlos', 'Doe'), new \DateTimeImmutable('4 years ago'), $departments[1], new BaseSalary(Money::USD(1999_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Romany', 'Kaur'), new \DateTimeImmutable('13 years ago'), $departments[1], new BaseSalary(Money::USD(2300_00))),
+            new Employee(Uuid::v4(), new EmployeeName('John', 'Fellows'), new \DateTimeImmutable('2 years ago'), $departments[4], new BaseSalary(Money::USD(1000_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Renesmae', 'Rich'), new \DateTimeImmutable('5 years ago'), $departments[3], new BaseSalary(Money::USD(1650_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Veronica', 'Doe'), new \DateTimeImmutable('15 months ago'), $departments[3], new BaseSalary(Money::USD(2150_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Carlos', 'Long'), new \DateTimeImmutable('15 months ago'), $departments[1], new BaseSalary(Money::USD(2190_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Alan', 'Coates'), new \DateTimeImmutable('20 months ago'), $departments[4], new BaseSalary(Money::USD(3100_00))),
+            new Employee(Uuid::v4(), new EmployeeName('Montell', 'Mcbride'), new \DateTimeImmutable('3 months ago'), $departments[0], new BaseSalary(Money::USD(1990_00))),
         ];
 
         foreach ($employees as $employee) {
